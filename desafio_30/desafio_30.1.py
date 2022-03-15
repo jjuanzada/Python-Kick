9da63eb168e3f80401b5dbf657cdefb5c6d0310f
jjuanzada <95050417+jjuanzada@users.noreply.github.com>
2022-03-15 13:13:03 -0300

    Add files via upload
    Desafios da aula 30

---
+++ desafio_30/desafio_30.1.py
@@ -0,0 +1,15 @@
+print("Calcula média")
+
+n1 = float(input("Digite a primeira nota : "))
+n2 = float(input("Digite a segunda nota : "))
+n3 = float(input("Digite a segunda nota : "))
+m = float
+
+m = (n1 + n2 + n3)/3
+
+print("A sua media é : ", m)
+
+if m<6:
+    print("Você não passou")
+else:
+    print("Você está APROVADO!")
