6f34c20ce460ba2fd62c3de7c97098eebf4ead22
jjuanzada <95050417+jjuanzada@users.noreply.github.com>
2022-03-15 13:11:23 -0300

    Add files via upload
    Desafios da aula 29

---
+++ desafio_29/desafio_29.2.py
@@ -0,0 +1,12 @@
+print("Faça seu cadastro")
+
+nome = str(input("Digite seu nome : "))
+email = str(input("Digite seu email : "))
+tel = int(input("Digite seu telefone(sem espaços e traços) : "))
+id = int(input("Digite sua idade : "))
+
+print("Seu cadastro : ")
+print(nome)
+print(id)
+print(email)
+print(tel)
