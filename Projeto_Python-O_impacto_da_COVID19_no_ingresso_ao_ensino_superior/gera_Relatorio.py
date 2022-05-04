cf85299356dffa6577617865ab9030f4a8822af5
jjuanzada <95050417+jjuanzada@users.noreply.github.com>
2022-05-04 15:15:30 -0300

    coleta dados do INEP e salva na planilha

--- Projeto_Python-O_impacto_da_COVID19_no_ingresso_ao_ensino_superior/gera_Relatorio.py
+++ Projeto_Python-O_impacto_da_COVID19_no_ingresso_ao_ensino_superior/gera_Relatorio.py
@@ -11,7 +11,7 @@ def ajustaMes(mes):
     meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
     return meses[int(mes)-1]
 
-# Verifica se a váriavel é do tipo inteiro
+# Verifica se o valor é do tipo inteiro
 def validaInt(str):
   try:
     int(str)
@@ -113,9 +113,55 @@ estiloGraficoBarh('Óbitos por mês - 2020 a 2022', 'Quantidade de óbitos', 'Da
 adicionaLegendaBarh(Obitos, 65)
 plt.savefig('Projeto_Python-O_impacto_da_COVID19_no_ingresso_ao_ensino_superior/obitosMensal.png', format='png', dpi=300)
 
-# Tamanho do PDF
-largura = 210
-altura = 297
+urlInsc2019 = requests.get('https://www.gov.br/inep/pt-br/assuntos/noticias/enem/mais-de-5-milhoes-de-participantes-estao-confirmados-para-a-edicao-do-exame-em-2019')
+contentInsc2019 = urlInsc2019.content
+
+siteInsc2019 = BeautifulSoup(contentInsc2019, 'html.parser')
+tbodyInsc2019 = siteInsc2019.find('tbody')
+
+urlInsc2020 = requests.get('https://www.gov.br/inep/pt-br/assuntos/noticias/enem/58-milhoes-estao-inscritos-para-fazer-o-enem-2020')
+contentInsc2020 = urlInsc2020.content
+
+siteInsc2020 = BeautifulSoup(contentInsc2020, 'html.parser')
+tbodyInsc2020 = siteInsc2020.find('tbody')
+
+urlInsc2021 = requests.get('https://www.gov.br/inep/pt-br/assuntos/noticias/enem/divulgados-os-numeros-de-inscritos-no-enem-2021-por-uf')
+contentInsc2021 = urlInsc2021.content
+
+siteInsc2021 = BeautifulSoup(contentInsc2021, 'html.parser')
+tbodyInsc2021 = siteInsc2021.find('tbody')
+
+insc2019=[]
+insc2020=[]
+insc2021=[]
+
+for x in tbodyInsc2019.findAll("tr"):
+    cells = x.findAll('td')
+    if len(cells)==3:
+        insc2019.append(cells[1].find('span'))
+
+for x in tbodyInsc2020.findAll("tr"):
+    cells = x.findAll('td')
+    if len(cells)==4:
+        insc2020.append(cells[3].find(text=True))
+
+for x in tbodyInsc2021.findAll("tr"):
+    cells = x.findAll('td')
+    if len(cells)==5:
+        insc2021.append(cells[4].find('p'))
+
+In2019 = insc2019[26].text
+In2020 = insc2020[25]
+In2021 = insc2021[25].text
+
+i1=['Ano','Inscrições']
+i2=[2019, In2019]
+i3=[2020, In2020]
+i4=[2021, In2021]
+
+planilha = pd.DataFrame(columns=[i1, i2, i3, i4])
+
+planilha.to_csv('Projeto_Python-O_impacto_da_COVID19_no_ingresso_ao_ensino_superior/Balanços_Enem-SP.csv', index=['ano', 2019,2020])
 
 # Cria o PDF
 pdf = FPDF('P', 'mm', 'A4')
@@ -125,17 +171,22 @@ pdf.add_page()
 
 pdf.set_font('Arial', 'B', 20)
 pdf.set_text_color(0, 0, 0)
-pdf.cell(0, 50, 'O impacto da COVID19 no ingresso ao ensino superior\nno estado de São Paulo', align='C', ln=1)
+pdf.cell(0, 40, 'O impacto da COVID19 no ingresso ao ensino superior', align='C', ln=1)
+pdf.cell(0, 0, 'no estado de São Paulo', align='C', ln=1)
 
 pdf.set_font('Arial', 'B', 16)
 pdf.set_text_color(0, 0, 0)
-pdf.cell(0, 155, 'Análise feita por:\nJuan Souza', align='C', ln=1)
+pdf.cell(0, 200, 'Juan Souza', align='C', ln=1)
 
 # Insere o titulo da segunda página
 pdf.add_page()
 pdf.set_font('Arial', 'B', 18)
 pdf.set_text_color(0, 0, 0)
 pdf.cell(0, 15, 'Dados da COVID19 em SP', align='C', ln=1)
+
+# Tamanho dos gráficos no PDF
+largura = 210
+altura = 297
 
 # Insere os gráficos dos óbitos e casos mensais
 pdf.image('Projeto_Python-O_impacto_da_COVID19_no_ingresso_ao_ensino_superior/casosMensal.png', 5, 33, largura-10)
